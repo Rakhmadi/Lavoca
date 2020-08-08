a0b516355b8f53c7845d3d06d54cfd7f9634b826
rakhmadi D <rakhmadiwalker@gmail.com>
2020-08-09 00:40:57 +0700

    push

--- routers/router.ts
+++ routers/router.ts
@@ -2,24 +2,22 @@ import {router,Route} from '../core/Router___.ts'
 import {gets} from '../controller/c.ts'
 import {BaseUrl} from '../core/app___.ts'
 import View from '../core/view___.ts'
-const { cwd, stdout, copy } = Deno;
-import * as dejs from 'https://deno.land/x/dejs/mod.ts';
 
 Route.Get("/",(ctx)=>{
     return View.make(ctx,"index")
 })
 
-Route.Get("/sss/:id",gets)
+Route.Get("/cek/:id",gets)
 
-Route.Post("/ssssssssssss",(ctx)=>{
+Route.Post("/cek",(ctx)=>{
     ctx.response.body =23423;
 })
 
-Route.Put("/ssssssssssss",(ctx)=>{
+Route.Put("/cek",(ctx)=>{
     ctx.response.body =23423;
 })
 
-Route.Delete("/ssssssssssss",(ctx)=>{
+Route.Delete("/cek",(ctx)=>{
     ctx.response.body =23423;
 })
 Route.Get("/view",async(ctx)=>{
