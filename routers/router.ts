b25d62683dd7901171b42be210d8250e0ee587f7
rakhmadi D <rakhmadiwalker@gmail.com>
2020-08-23 23:15:16 +0700

    fixerr

--- routers/router.ts
+++ routers/router.ts
@@ -2,8 +2,8 @@ import {router,Route,Middleware} from '../core/Router___.ts'
 import {gets,data} from '../controller/c.ts'
 import {BaseUrl} from '../core/app___.ts'
 import View from '../core/view___.ts'
-import { example,x } from '../middleware/examplemiddleware.ts'
 
+import { example,x,cc,nn } from '../middleware/_Kernel.ts'
 import { ControllerRoute } from '../core/controller___.ts'
 
 Route.get("/",(ctx)=>{
@@ -12,11 +12,22 @@ Route.get("/",(ctx)=>{
 Route.get("/data",data)
 Route.get("/cek/:id",gets)
 Route.get("/df",await ControllerRoute.add("i", "show"))
+
 Route.middleware([example],()=> {
     Route.get('/ns', (ctx) => {
         ctx.response.body= 'dsf'
-    },[x])
+    },[x,cc,nn])
 })
+Route.prefix('/v1',()=>{
+    Route.prefix('/name',()=>{
+        Route.get('/rakhmadi',(ctx)=>{
+            ctx.response.body = 'rtrtrtrt'
+        },[x])
+    })
+})
+Route.get("/b/:id",gets)
+
+
 
 let listrouter = Route.ShowRoute()
 export  {router,listrouter}
